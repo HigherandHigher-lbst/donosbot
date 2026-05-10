840b2032e13870e7087abd35ee9bcfd316ca159b
HigherandHigher-lbst <wqnmq4qyww@privaterelay.appleid.com>
2026-05-10 23:35:09 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import telebot
 
 TOKEN = '8225110405:AAFJd_G3lAR4j8rDvhGciIwCVBW6AwCrfws'
 TARGET_CHAT_ID = -1003796818229  
-ADMIN_CHAT_ID = 5110146436
-MESSAGE_THREAD_ID = 3021
+ADMIN_CHAT_ID = 5110146436        
+TOPIC_ID = 3021                   
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -11,19 +11,21 @@ bot = telebot.TeleBot(TOKEN)
 def start(message):
     bot.send_message(message.chat.id, "Приветик! Хочешь написать донос? Пиши сюда ниже👇")
 
-
 @bot.message_handler(func=lambda message: message.chat.type == 'private')
 def handle_private_message(message):
     if message.text:
         
-        bot.send_message(TARGET_CHAT_ID, message.text)
-        
-        
-        username = f"@{message.from_user.username}" if message.from_user.username else "нет username"
+        bot.send_message(
+            chat_id=TARGET_CHAT_ID,
+            text=message.text,
+            reply_to_message_id=TOPIC_ID
+        )
+    
+    
+    username = f"@{message.from_user.username}" if message.from_user.username else "нет username"
     info = f"Донесено от:\nID: {message.from_user.id}\nUsername: {username}"
     bot.send_message(ADMIN_CHAT_ID, info)
     
-    
-    bot.send_message(message.chat.id, "Донос успешно отправлен✅!")
+    bot.send_message(message.chat.id, "Донос успешно отправлен✅")
 
 bot.polling(none_stop=True)   
