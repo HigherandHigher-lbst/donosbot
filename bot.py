4d5c0f6060ee6115995527d7aca122746075d104
HigherandHigher-lbst <wqnmq4qyww@privaterelay.appleid.com>
2026-05-10 22:52:12 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,31 @@
 import telebot
 
-TOKEN = '8225110405:AAE6PO-F3GVDxRzbd2LJqUTBLaxcpLY2BXw'
-TARGET_CHAT_ID = -1003796818229  
-ADMIN_CHAT_ID = 5110146436         
+TOKEN = '8225110405:AAGYJ_xFdgSywGJMaUeTjAn_fhAeK6wYQhE'
+TARGET_CHAT_ID = -1003796818229 
+MESSAGE_THREAD_ID = 28
+ADMIN_CHAT_ID =  5110146436            
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Приветик! Хочешь написать донос? Пиши ниже👇(за фейк будет варн!).")
+    bot.send_message(message.chat.id, "Привет! Хочешь написать донос? Пиши его ниже (за фейк донос варн) .")
 
 @bot.message_handler(func=lambda m: True)
 def handle_message(message):
+    if message.text:
+        
+        bot.send_message(
+            chat_id=TARGET_CHAT_ID,
+            text=message.text,
+            message_thread_id=MESSAGE_THREAD_ID
+        )
     
-    bot.send_message(TARGET_CHAT_ID, message.text)
     
-    
-    info = f"Донесено от:\nID: {message.from_user.id}\nUsername: @{message.from_user.username}"
+    username = f"@{message.from_user.username}" if message.from_user.username else "нет username"
+    info = f"Донесено от:\nID: {message.from_user.id}\nUsername: {username}"
     bot.send_message(ADMIN_CHAT_ID, info)
-
     
-    bot.send_message(message.chat.id, "✅ Отправлено.")
+    bot.send_message(message.chat.id, "Донос успешно отправлен!.")
 
-bot.polling(none_stop=True)
+bot.polling(none_stop=True)   
