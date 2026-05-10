14330bd481370441642d2dc0b470971358f84dd6
HigherandHigher-lbst <wqnmq4qyww@privaterelay.appleid.com>
2026-05-10 22:05:37 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,38 +1,25 @@
-import os
 import telebot
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import threading
 
-TOKEN = "8225110405:AAE6PO-F3GVDxRzbd2LJqUTBLaxcpLY2BXw"
-GROUP_ID = -1003796818229 
-TOPIC_ID = 11             
-ADMIN_ID = 5110146436     
+TOKEN = '8225110405:AAE6PO-F3GVDxRzbd2LJqUTBLaxcpLY2BXw'
+TARGET_CHAT_ID = -1003796818229  
+ADMIN_CHAT_ID = 5110146436         
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(func=lambda message: message.chat.type == 'private')
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Приветик! Хочешь написать донос? Пиши ниже👇(за фейк будет варн!).")
+
+@bot.message_handler(func=lambda m: True)
 def handle_message(message):
-        
-    bot.copy_message(
-        chat_id=GROUP_ID,	
-            
-        from_chat_id=message.chat.id,
-        message_id=message.message_id,
-        message_thread_id=TOPIC_ID
-    )
     
-    auth_info = f"Автор: {message.from_user.first_name} | ID: {message.from_user.id}"
-    bot.send_message(ADMIN_ID, f"Новый донос от {auth_info}")
+    bot.send_message(TARGET_CHAT_ID, message.text)
     
-)
-    bot.forward_message(ADMIN_ID, message.chat.id, message.message_id)
     
-    bot.send_message(message.chat.id, "Отправлено.")
+    info = f"Донесено от:\nID: {message.from_user.id}\nUsername: @{message.from_user.username}"
+    bot.send_message(ADMIN_CHAT_ID, info)
 
-def run_server():
-    port = int(os.environ.get("PORT", 10000))
-    server = HTTPServer(('0.0.0.0', port), type('H', (BaseHTTPRequestHandler,), {'do_GET': lambda s: (s.send_response(200), s.end_headers())}))
-    server.serve_forever()
+    
+    bot.send_message(message.chat.id, "✅ Отправлено.")
 
-threading.Thread(target=run_server, daemon=True).start()
-bot.polling(none_stop=True)
+bot.polling()
